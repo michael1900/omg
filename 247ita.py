dc45238b2e0e01ab18f2a61f362c0d651fddcd14
ciccioxm3 <cicciariello@proton.me>
2025-05-15 14:26:10 +0200

    Update 247ita.py

--- 247ita.py
+++ 247ita.py
@@ -377,9 +377,9 @@ def add_dazn1_channel():
     if stream_url_dynamic:
         with open(M3U8_OUTPUT_FILE, 'a', encoding='utf-8') as file:
             file.write(f"#EXTINF:-1 tvg-id=\"{tvg_id}\" tvg-name=\"{channel_name}\" tvg-logo=\"{tvicon_path}\" group-title=\"{category}\", {channel_name} (D)\n")
-            file.write(f'#EXTVLCOPT:http-referrer=https://caq21harderv991gpluralplay.xyz/\n')
+            file.write(f'#EXTVLCOPT:http-referrer=https://{REFERER}/\n')
             file.write('#EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36\n')
-            file.write('#EXTVLCOPT:http-origin=https://caq21harderv991gpluralplay.xyz\n')
+            file.write(f'#EXTVLCOPT:http-origin=https://{ORIGIN}\n')
             file.write(f"{stream_url_dynamic}\n\n")
             return 1
     else:
